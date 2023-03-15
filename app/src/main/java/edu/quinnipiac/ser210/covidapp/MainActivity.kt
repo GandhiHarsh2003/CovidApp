db940aa4064bb5c5db376231ddbde43ed9a48858
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2023-03-15 19:52:21 -0400

    added toolbar

--- app/src/main/java/edu/quinnipiac/ser210/covidapp/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/covidapp/MainActivity.kt
@@ -2,10 +2,37 @@ package edu.quinnipiac.ser210.covidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.items_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.changeColor -> {
+                Toast.makeText(this, "first item", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
 }
